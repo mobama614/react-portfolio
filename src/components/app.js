cea999fc1fdfd1dec469d8f78feda07902a6a0aa
mohammed-ali <organifiselct@gmail.com>
2021-04-30 15:41:52 -0700

    Overview of Authentication Behavior

--- src/components/app.js
+++ src/components/app.js
@@ -7,18 +7,20 @@ import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
 import PortfolioDetail from "./portfolio/portfolio-detail";
+import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
   render() {
     return (
-      <div className="app">
+      <div className="container">
         <Router>
           <div>
             <NavigationContainer />
 
             <Switch>
               <Route exact path="/" component={Home} />
+              <Route path="/auth" component={Auth} />
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
